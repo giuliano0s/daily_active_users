8f3498f348a1dd132314732288fbea9e515bbb38
Giuliano3530 <giuliano.souza@gavb.com.br>
2025-05-28 11:03:39 -0300

    Case terminado

---
+++ run_mlflow.bat
@@ -0,0 +1,6 @@
+@echo off
+cd /d %~dp0
+call conda activate base-plus
+mlflow ui --backend-store-uri file:models/mlruns --port 5000
+REM Abre inicialmente uma aba com erro (delay de segundos até o mlflow abrir)
+start http://127.0.0.1:5000
